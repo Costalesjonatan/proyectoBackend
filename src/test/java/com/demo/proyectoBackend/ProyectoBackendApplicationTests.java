2cbc40fa9cb2fb9204da95db214217175de799f2
Costalesjonatan <42694800+Costalesjonatan@users.noreply.github.com>
2020-12-07 13:44:10 -0300

    commit inicial

---
+++ src/test/java/com/demo/proyectoBackend/ProyectoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.proyectoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
