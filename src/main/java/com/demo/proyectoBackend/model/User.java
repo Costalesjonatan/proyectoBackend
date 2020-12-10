4538d89d4f4bb07685fdc5962a98b66fcd901b8f
Costalesjonatan <42694800+Costalesjonatan@users.noreply.github.com>
2020-12-10 09:39:36 -0300

    Lombok y refactorización del user

--- src/main/java/com/demo/proyectoBackend/model/User.java
+++ src/main/java/com/demo/proyectoBackend/model/User.java
@@ -7,12 +7,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private int id;
 	
 	@Column(name = "nombre")
@@ -24,55 +35,4 @@ public class User {
 	@Column(name = "password")
 	private String password;
 	
-	public User()
-	{
-		
-	}
-	
-	public User(String nombre, String apellido, String email, String password){
-		this.setNombre(nombre);
-		this.setApellido(apellido);
-		this.setEmail(email);
-		this.setPassword(password);
-	}
-	
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
